8d8587d08c66a18c8fca3d3f7c399bebba4bb429
pfeerick <pfeerick@users.noreply.github.com>
2016-12-28 11:39:42 +1000

    Added OakUpdater2 files
    Added the OakUpdater2 files as is from the wiki. This was the second revision to fix up a missing line from the linux updater script.

---
+++ update.bat
@@ -0,0 +1,8 @@
+@echo off
+set /p comport="Enter COM Port of USB-Serial Adapter: "
+echo Ensure Oak is attached as shown here and press a key: digistump.com/wiki/oak/serialupdate
+pause
+echo Updating...
+esptool --baud 115200 --port %comport% write_flash -fs 32m 0x1000 blank.bin 0x2000 firmware_v1.bin 0x101000 blank.bin 0x102000 blank.bin 0x202000 blank.bin 
+echo Done
+pause
